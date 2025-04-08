ee679a03e0c32afffb6169b7303d82914c0f9d7a
Ixtiyorxon <xabibulloyevixtiyorxon@gmail.com>
2025-04-08 11:54:30 +0500

    Problem2235

--- src/main/java/org/example/Problem2235.java
+++ src/main/java/org/example/Problem2235.java
@@ -2,10 +2,11 @@ package org.example;
 
 public class Problem2235 {
     public static void main(String[] args) {
-        System.out.println(sum(-10,4));
+        System.out.println(sum(-10, 4));
     }
+
     public static int sum(int num1, int num2) {
-        return num1+num2;
+        return num1 + num2;
     }
 }
 
